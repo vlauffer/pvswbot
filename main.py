02e3049a8307510ec307141ada7df04a57df46ad
Vince Lauffer <vlauffer@gmail.com>
2022-04-21 22:03:06 -0400

    spam method

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 
 import requests
-import json
 import discord
 from discord.ext import tasks
 import os
 import emoji
 import datetime
-
 
 token =""
 prod_url = "https://pvswbot-backend.herokuapp.com"
@@ -18,7 +16,15 @@ add_reaction_url = "/reaction/add"
 remove_reaction_url = "/reaction/remove"
 active_url=""
 
-# sets request url and token, which is determined by the existance of the secrets file
+lorem = ["Enim 📰🍢🍩🕝🍕🌄🐸🕙🎱🎳📳💹 proin 💝🔨🎺📈🗽🍭👰 in 🔹🔞🔴👒🕔🔂🕛 nulla enim 🌕🕠📏🔺 magna pharetra nunc, 🍑📠🔵🍯🍀🍛 sit condimentum non risus 👇🕕🏆🏫🐺 🎻🏯👸🎧🔥 orci nunc 👾🍎🌉🎶🔦 libero venenatis, augue aenean ultrices 🎋🏰🐚🎹🐋💆🌍📪🎭🔆🎮💪👀🎅 🍳🔲🍚🎨🌵🎑👟🔈🎀👔🕝🎁 👼🐼💐🐴 🐈💽👓📬📰🎳 volutpat facilisis turpis 👈📖💬📫🔨 convallis quam 👛👇📬💑🍗🌠💉💒 🔗📓🍭🐼👱📫🍐🌕 ut 👋🎴🐕🗻 accumsan 🍢🐠🐥📂🔵🎇🔱🎺🌆 🎾🎴📳🍓🕧🐓🐨🏈🔄🔄 in convallis 🐹🔆👠🐩📘📲🎒🐟📷🐢🐒💓🎀 proin viverra sit vulputate 🍄🕢🐰💃🎈 interdum 👜📮🕥👑🍄🐐🍀🍸🕚 🕖🔆🎈🌐🐜 👢👹🍶💿🎾👏💾🏃🐌🔷 enim aliquet 🍨🍹🕙🔈🎶🐸 tristique 🍩👨🐵👢🌑 ac, elementum, curabitur imperdiet 🍥💼🗽🏭🌍 nunc, scelerisque facilisis vestibulum 📇🎍📒🐖🕗🕕📓 arcu 📟🔪👴📗 📯💸👱🐉🍚 🔥🍍📼👕👍🕝🔂💶 nunc, justo nunc at 📓🌱🎍👝🎹🔊🔹👐 🌕📫🏨🎁🕚🐖🔓🐇🎷🌷🔰🐜📣🎋 donec 🔈💜🍗🔱🎻🍏🌏🌓🍆💃🍢🏯🐐 natoque fermentum 🍱🔥🌰🏬📳🍳 non in. 🏤🏩🎪👟🍻👐🎧🔲🔟🎺📊 💃🗽📓🔴 nunc et non",
+"dictum dui, malesuada magna 🐲🏣🔰👻🐷🌱👢 pellentesque duis 💐💔📠👸🕤 📲🐕🏮📹🏭🐦🏮📏🍆🏠🔌💽🏪🎧 sed 🌽🏭🌺🎷 🐣🌵💅👛🔟🔨 🔋🐺💦🐊🎺 🍚📯🎈📃 quis 💤🐰🐠📬 tempor ullamcorper viverra massa 🕑📆🌀👎🔑💒🐒🍄🎧🍙🍪🕁🍰👐 nulla semper 🍏🎺🌟🐏 👤🍅🔲👄🎧📣🌵🍂 consectetur in 💫🍜🐕🌰 curabitur 📓🌏🌖📩🔫👚🌵🔣🔤🏬🍎🌽👅🎿 🍦📄📥📕🔩🏦🐚🎷 🎦💌🔭🕤🐢🐏🍀🕘🏂👌📺🔡🔼 🍈🕁🐖🏣🕞🐒 🏥🔇💃💬🍦👻 🐟🏤🌾👩📋🕤🎢💉📠🗾 🗻🔯💠🐝🌀🎣 posuere 💆🏤👢🔯🐌💸 lobortis 🔁🎣🌏🎱💈💔 📠👑🎱🔙 proin 📝🔨🍬🔂🍍🔭🔮🔻🔚🌠🎤🍶🔇. Semper adipiscing risus 🔷🔜🍄🔟🔂🕜🌁🍔🌔🐺🐼🔌 nibh et 🍑👬🍵",
+"👻📠🐢🌲🎪🏊🔲🕘🍼🌹🐡💱🍆 vitae 💼🎎🎮🐛 👦🕀👓🐣🌇📥🐰🎂👦🔀🏰🔴 🍪💒🏉📤🏦📔🔼💬🔤🎹👳 🔜🌕🏦🔴 🐁🍥🏇🏫 gravida enim pellentesque eget maecenas bibendum arcu 🐚🔥🍫🔻💼💦💼🐍👙🎑 ullamcorper 🐗🔥👲🕕🐼💖 tortor sed in 🐒📖🍷👨 lacus, pharetra blandit 🕗🏊📱🐯👏💄👄 elit lectus 🍳👘🔓🌝💓🍘💺🎄🏠🔡🔔🎰🔛🔬 💅📐👥💜💇🐎🕦 🍌🌆🌵📯👋💎🕔🍒 🗽🔛🐜🐽🌁💫💍 magna auctor 📍🐖👉🎭📯📝🌾📁🔀🔥🔍🔸🐊🔤💐 🔜🔙📜🍡💊🌏 vulputate proin id vestibulum, vel natoque 🔞🎨🍩🎃👖📞🔲 🕜🍶📰🐒🍁🌉🕁 💖👰💭🔌🍌💷💯👺🍫🍙🌰🎡🐋🐧💭🏡🌘🍥 🐩🔸🔆🐪💢🎌🍎🌼🔙🔘🍸🔥 eget pulvinar cursus 💸📮🎲📳 vitae 📑",
+"📊📬💺🐛🐇 🍏🌌🔰👜 varius lobortis 💞🕐📈📝 sed amet, nisl 🍪🔨📟🔍💅📢👖🎌 laoreet mi 📨💗👒🐢👤📈🏮🏫📝🗾🔄🌺🐙🍗📦📆🌑📟🍳 🔼🍧🔑🗻👞🍂📻👄🏣👫🐴🐷🌺💸🕣 tristique 🕗👷🐭👊🎷🎍 in 🎎💊📇📋💪🐑 sed vitae 🕚📻👮🐊📭📜 lectus 💇📁🏫🌔💝🌿🏩👺 dapibus hendrerit 🎃🐏💀👒🕙🔞🌖 💲🐸👛🔘🕗 in 🐙🔐🕀🐵👱🍼 quis quisque ullamcorper sociis. Amet, 🐁🎬📫🌲💚🌘🍶📣👺🔌 tempor 👬🔲💱🐸🌚🍤 faucibus 🎵🐻🎓🎅🗾💑👣👆🌖🌎 adipiscing purus 🔍🐓💹👱👌🍜📓📯🍳🐡👳🎸🎱🔘 auctor sed 📫📁🐏",
+"🎠🐱🎵💍💸🎶🔩👔🔔🔷🍑 🎬🍩🌟👘📝🐚📢🍦🎂📫 🎲🎃🌘📁💿🏧👮🍮📕💮🎻🌹💬🔵🔀 🎇💽🕜🍔🍘👛 🎾🎃🎃🍛🎼🌅🏤💫🐖🌏🍦🕜🌿🍱🗽🍖 🌲🌲🌸🌳💶🔵🔕 sollicitudin vivamus 🏢🐃🐥🍷📆🕛🌼🐸🔀🌽🍞👤💫🍼 ipsum tempus 🔙🔭💪🎨🐎🍷🍼🐚🎑🐮🔱🌹🎨 suscipit nisi 💅🌲🐕🐮🎫👸🔆🎿 consectetur id vestibulum nunc adipiscing egestas consectetur convallis id lacus est et."
+]
+
+# sets request url and token, which is determined by the existance of the secrets file.
+# if no secrets file is found, sets active_url to production url and token to the discord bot token found in heroku.
 try:
   from secrets import TOKEN
 except:
@@ -32,69 +38,14 @@ else:
   active_url=local_url
  
 
-# interval in which to get new messages
+# the amount of seconds between each call of get_all_messages. This wait time is set in looper()
 time_delta = 10
 
+# creation of the discord client
 client = discord.Client()
 
-# sends array of message objects to the backend for insertion
-def insert_messages(messages):
-  
-  print("Inserting pancakes (and other emojis)")
-
-  data = {'messages': messages}
-  try:
-    response = requests.post(active_url+insert_url, timeout=5, json = data)
-    print(response)
-
-  except requests.exceptions.RequestException as e:
-    print (e)
-
-# Future functionality: inserting reactions into db
-def insert_reaction(payload):
-  reaction_struct = {
-    # "username": message.author.display_name +"#"+ message.author.discriminator,
-    "user_id": payload.user_id,
-    "channel_id": payload.channel_id,
-    "message_id": payload.message_id,
-    "content": payload.emoji.name,
-    "created_at": datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
-  }
-
-  print("Inserting reactions")
-
-  data = {'reaction': reaction_struct}
-  try:
-    response = requests.post(active_url+ add_reaction_url, timeout=5, json = data)
-    print(response)
-
-  except requests.exceptions.RequestException as e:
-    print (e)
-  print(payload.emoji.name)
-
-#checks for flag
-def is_flag_emoji(c):
-  return "\U0001F1E6\U0001F1E8" <= c <= "\U0001F1FF\U0001F1FC" or c in ["\U0001F3F4\U000e0067\U000e0062\U000e0065\U000e006e\U000e0067\U000e007f", "\U0001F3F4\U000e0067\U000e0062\U000e0073\U000e0063\U000e0074\U000e007f", "\U0001F3F4\U000e0067\U000e0062\U000e0077\U000e006c\U000e0073\U000e007f"]
-
-# checks to see if there is an emoji in a string
-def text_has_emoji(text):
-  
-
-
-  
-  
-
-  for character in text:
-    flagcheck = is_flag_emoji(character)
-    if character in emoji.UNICODE_EMOJI['en'] or flagcheck:
-      # emojilist = 
-      
-      return True
-  return False
-
-
 # gets all messages within a given time frame (determined by time_delta), 
-# creates object that contains message information, and prompts insert_messages()
+# creates object that contains message information, and prompts send_inserted_messages()
 async def get_all_messages():
 
   messages_to_send =[]
@@ -105,11 +56,10 @@ async def get_all_messages():
 
   # for every channel in the Discord guild, test to see if the channel is a Text Channel. 
   # if the channel is a Text Channel, get messages within a given time interval from now, and add them
-  # ti the messages_to_send array
+  # to the messages_to_send array
   for channel in all_channels:
     if type(channel).__name__=='TextChannel':
       try: 
-        # channel_history = await channel.history(limit=5).flatten()
         channel_history = await channel.history(limit=None, after=time_to_get_messages).flatten()
         for message in channel_history:
           if text_has_emoji(message.content):
@@ -125,18 +75,95 @@ async def get_all_messages():
       except:
         continue
   if len(messages_to_send)>0:
-    insert_messages(messages_to_send)
+    send_inserted_messages(messages_to_send)
 
-def remove_reaction(payload):
+# sends array of message objects to the backend for insertion
+def send_inserted_messages(messages):
+  
+  data = {'messages': messages}
+  try:
+    response = requests.post(active_url+insert_url, timeout=5, json = data)
+    print(response)
+
+  except requests.exceptions.RequestException as e:
+    print (e)
+
+# Called whenever a message is edited. creates message object and invokes send_edited_message to send message to the backend
+@client.event
+async def on_raw_message_edit(payload):
+  message = {
+    "username": payload.data['author']['username'],
+    "user_id": payload.data['author']['id'],
+    "content": payload.data['content'],
+    "channel_id": payload.channel_id,
+    "message_id": payload.message_id,
+    "created_at":  datetime.datetime.fromisoformat(payload.data['timestamp']).strftime("%Y-%m-%d %H:%M:%S")
+  }
+  send_edited_message(message)
+  
+# Sends an edited message to the backend
+def send_edited_message(message):  
+  data = {'message': message}
+  try:
+    response = requests.post(active_url+edit_url, timeout=5, json = data)
+    print(response)
+
+  except requests.exceptions.RequestException as e:
+    print (e)
+
+#called every time a message is deleted, and invokes send_deleted_message
+@client.event
+async def on_raw_message_delete(payload):
+  send_deleted_message(payload.message_id)
+
+#sends a message_id to the backend for deletion
+def send_deleted_message(message_id):
+  data = {'message_id': message_id}
+  try:
+    response = requests.post(active_url+delete_url, timeout=5, json = data)
+    print(response)
+
+  except requests.exceptions.RequestException as e:
+    print (e)
+
+# called whenever a reaction is added, regardless of its cached value.
+# invokes Send a reaction to the backend
+@client.event
+async def on_raw_reaction_add(payload):
+  send_added_reaction(payload)
+
+# future functionality: inserting reactions into db
+def send_added_reaction(payload):
   reaction_struct = {
-    # "username": message.author.display_name +"#"+ message.author.discriminator,
+    "user_id": payload.user_id,
+    "channel_id": payload.channel_id,
+    "message_id": payload.message_id,
+    "content": payload.emoji.name,
+    "created_at": datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+  }
+
+  data = {'reaction': reaction_struct}
+  try:
+    response = requests.post(active_url+ add_reaction_url, timeout=5, json = data)
+    print(response)
+
+  except requests.exceptions.RequestException as e:
+    print (e)
+  print(payload.emoji.name)
+
+
+# called whenever a reaction is added, regardless of its cached value.
+@client.event
+async def on_raw_reaction_remove(payload):
+  send_removed_reaction(payload)
+
+# send a reaction to the backend for deletion
+def send_removed_reaction(payload):
+  reaction_struct = {
     "user_id": payload.user_id,
     "message_id": payload.message_id,
     "content": payload.emoji.name
-    
   }
-
-  print("removing reaction")
 
   data = {'reaction': reaction_struct}
   try:
@@ -148,85 +175,61 @@ def remove_reaction(payload):
   print(payload.emoji.name)
 
 
-# initializes the bot and calls the looper function in order to start fetching messages at a given interval
-@client.event
-async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
-  # insert_messages("messages_to_send")
-  await looper.start()
+#checks if character is a component of a flag emoji
+def is_flag_emoji(c):
+  return "\U0001F1E6\U0001F1E8" <= c <= "\U0001F1FF\U0001F1FC" or c in ["\U0001F3F4\U000e0067\U000e0062\U000e0065\U000e006e\U000e0067\U000e007f", "\U0001F3F4\U000e0067\U000e0062\U000e0073\U000e0063\U000e0074\U000e007f", "\U0001F3F4\U000e0067\U000e0062\U000e0077\U000e006c\U000e0073\U000e007f"]
+
+# checks to see if there is an emoji in the text
+def text_has_emoji(text):
+  for character in text:
+    flagcheck = is_flag_emoji(character)
+    if character in emoji.UNICODE_EMOJI['en'] or flagcheck:
+      return True
+  return False
+
+async def spam(all_channels ):
+  counter = 0
+  for channel in all_channels:
+    if type(channel).__name__=='TextChannel':
+      try: 
+        counter = counter+1
+        await channel.send(lorem[counter%5])
+        await channel.send(lorem[counter%5])
+        await channel.send(lorem[counter%5])
+        await channel.send(lorem[counter%5])
+        await channel.send(lorem[counter%5])
+        await channel.send(lorem[counter%5])
+
+      except:
+        continue
+  return
 
 # Event that helps us track if the bot is online (will be removed in a production environment)
 @client.event
 async def on_message(message):
   if message.author == client.user:
     return
-    
-  print(text_has_emoji(message.content))
+
+  if message.content.startswith("$b"):
+    #discord.utils.find() or just use str.find(message.content,"🥞")
+    varia = message.guild
+    await spam(varia.channels)
 
   if message.content.startswith("🥞"):
     #discord.utils.find() or just use str.find(message.content,"🥞")
-    await message.channel.send("+2000 pancake points")
+    await message.channel.send("+10 pancake points")
     
   if message.content.startswith("🧇"):
     await message.channel.send("+10 waffle points") 
 
 
-# Called whenever a message is edited
+# initializes the bot and calls the looper function in order to start fetching messages at a given interval
 @client.event
-async def on_raw_message_edit(payload):
-  date = datetime.datetime.fromisoformat(payload.data['timestamp']).strftime("%Y-%m-%d %H:%M:%S")
-  message = {
-    "username": payload.data['author']['username'],
-    "user_id": payload.data['author']['id'],
-    "content": payload.data['content'],
-    "channel_id": payload.channel_id,
-    "message_id": payload.message_id,
-    "created_at":  datetime.datetime.fromisoformat(payload.data['timestamp']).strftime("%Y-%m-%d %H:%M:%S")
-  }
-  edit_messages(message)
-  
-# Sends an edited message to the backend
-def edit_messages(message):  
-  print("Editing pancakes (and other emojis)")
-  data = {'message': message}
-  try:
-    response = requests.post(active_url+edit_url, timeout=5, json = data)
-    print(response)
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+  await looper.start()
 
-  except requests.exceptions.RequestException as e:
-    print (e)
-
-#called every time a message is deleted
-@client.event
-async def on_raw_message_delete(payload):
-  delete_message(payload.message_id)
-
-#sends a message to be deleted to the backend
-def delete_message(message_id):
-  print("delete message %d" % message_id)
-
-  data = {'message_id': message_id}
-  try:
-    response = requests.post(active_url+delete_url, timeout=5, json = data)
-    print(response)
-
-  except requests.exceptions.RequestException as e:
-    print (e)
-
-# Send a reaction to the backend
-@client.event
-async def on_raw_reaction_add(payload):
-  insert_reaction(payload)
-
-# Send a reaction to the backend for deletion
-@client.event
-async def on_raw_reaction_remove(payload):
-  remove_reaction(payload)
-
-
-
-
-# loops a given function, in this case, get_all_messages(), at given interval determined by time_delta (seconds)
+# loops the get_all_messages() function at given interval determined by time_delta (seconds)
 @tasks.loop(seconds=time_delta)
 async def looper():
   await get_all_messages()
